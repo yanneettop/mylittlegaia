fa319b48d9ccbbbd7773e01e47df3ae613c5d0dd
yanneettop <chesterlp0@gmail.com>
2026-05-25 16:44:34 +0100

    Commit Cloudflare Pages static output

---
+++ out/_next/static/kA4qwWaLtRJfjx3mxS7qT/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[slug]","\u002Fcrystal-library\u002F[slug]","\u002Fshop\u002F[slug]","\u002Fshop\u002Fcategory\u002F[slug]","\u002Fshop\u002Fintention\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
